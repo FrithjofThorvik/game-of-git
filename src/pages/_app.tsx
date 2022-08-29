db4b2cafb377ec4b12c51dc3fdb358f5f16537be
Frithjof Thorvik <frithjot@stud.ntnu.no>
2022-08-29 14:59:38 +0200

    :sparkles: #6 add loader to page change actions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,33 @@
 import type { AppProps } from 'next/app';
+import { Router } from 'next/router';
+import { Circles } from 'react-loader-spinner';
+import { useState } from 'react';
 import Layout from './components/layout/Layout';
 
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  // Activate loading when route is changed
+  Router.events.on('routeChangeStart', () => {
+    setIsLoading(true);
+  });
+
+  // Deactivate loader when route change is completed
+  Router.events.on('routeChangeComplete', () => {
+    setIsLoading(false);
+  });
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      {isLoading ? (
+        <div className="flex justify-center items-center w-full h-full">
+          <Circles color="#132F4C" height={40} width={40} />
+        </div>
+      ) : (
+        <Component {...pageProps} />
+      )}
     </Layout>
   );
 }
